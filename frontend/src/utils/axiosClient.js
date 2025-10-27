95257af8dff45cd9cc644b6d5cd8c7c4ada491cb
Parth <parthchauhan348@gmail.com>
2025-10-27 18:01:17 +0530

    Frontend Deployment

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: import.meta.env.VITE_API_URL||'/coding-platform-8i5t.vercel.app/api',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
